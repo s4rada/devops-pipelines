ee0b8675a1ce8526a087db17a0c545ec5b1ce347
Ron <iwayronmichael@gmail.com>
2025-09-19 01:17:37 +0800

    CL pipelines

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('Hello World!, i am testing cl pipelines');
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log('Starting in 3 seconds');
+setTimeout(() => {
+    console.log("wait 3 seconds");
+}, 3000);
+
+console.log('Test Finished!');
